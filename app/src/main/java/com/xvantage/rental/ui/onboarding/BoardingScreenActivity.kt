ffe8f86f444b93ebaaae58206609e1a85ff1457d
vipul_sapra <vipul.softnoesis@gmail.com>
2024-11-16 10:41:57 +0530

    onboarding view dummy view

--- app/src/main/java/com/xvantage/rental/ui/onboarding/BoardingScreenActivity.kt
+++ app/src/main/java/com/xvantage/rental/ui/onboarding/BoardingScreenActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.xvantage.rental.R
+import com.xvantage.rental.databinding.ActivityBoardingScreenBinding
+import com.xvantage.rental.databinding.ActivitySplashBinding
+import com.xvantage.rental.utils.AppPreference
 
 class BoardingScreenActivity : AppCompatActivity() {
+
+    private lateinit var layoutBinding: ActivityBoardingScreenBinding
+    lateinit var appPreference: AppPreference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_boarding_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        layoutBinding = DataBindingUtil.setContentView(this, R.layout.activity_boarding_screen)
+        appPreference = AppPreference(this)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
     }
 }
