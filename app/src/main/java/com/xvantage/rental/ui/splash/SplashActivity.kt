e5a0008199d9eff0536505d39e85a5ed1e080e52
vipul_sapra <vipul.softnoesis@gmail.com>
2024-12-10 17:38:53 +0530

    fixed bottom navigation dimensions

--- app/src/main/java/com/xvantage/rental/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/xvantage/rental/ui/splash/SplashActivity.kt
@@ -28,21 +28,25 @@ class SplashActivity : AppCompatActivity() {
         appPreference = AppPreference(this)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (appPreference.isUserLoginFirstTime()) {
-                val intent = Intent(this, DashboardActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
-            } else if (!appPreference.isFirstTimePreview()) {
-                val intent = Intent(this, BoardingScreenActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
-            } else{
-                val intent = Intent(this, SignUpActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
-            }
-        }, 2000)
+        val intent = Intent(this, DashboardActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            if (appPreference.isUserLoginFirstTime()) {
+//                val intent = Intent(this, DashboardActivity::class.java)
+//                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//                startActivity(intent)
+//            } else if (!appPreference.isFirstTimePreview()) {
+//                val intent = Intent(this, BoardingScreenActivity::class.java)
+//                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//                startActivity(intent)
+//            } else{
+//                val intent = Intent(this, SignUpActivity::class.java)
+//                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//                startActivity(intent)
+//            }
+//        }, 2000)
 
     }
 }
