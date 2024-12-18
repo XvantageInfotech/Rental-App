835a0646f19adae03d30fc64b957ccb9c3dd5e54
vipul_sapra <vipul.softnoesis@gmail.com>
2024-12-18 22:05:47 +0530

    Added Bordered Box

--- app/src/main/java/com/xvantage/rental/ui/addProperty/AddPropertyActivity.kt
+++ app/src/main/java/com/xvantage/rental/ui/addProperty/AddPropertyActivity.kt
@@ -1,7 +1,13 @@
 package com.xvantage.rental.ui.addProperty
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +31,71 @@ class AddPropertyActivity : AppCompatActivity() {
         appPreference = AppPreference(this)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         layoutBinding.toolbar.tvTitle.setText(R.string.add_property_bottom_n)
+        initView()
+        clickEvents()
+    }
+
+    private fun clickEvents() {
+        layoutBinding.toolbar.back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun initView() {
+        dropDownSpinner()
+
+    }
+
+    private fun dropDownSpinner() {
+        val spinner = findViewById<Spinner>(R.id.spinner_property_type)
+        val propertyTypes = listOf("Select Property Type", "House", "Apartment","PG","Rent House","Land")
+        val adapter = object : ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_item,
+            propertyTypes
+        ) {
+            override fun isEnabled(position: Int): Boolean {
+                return position != 0
+            }
+
+            override fun getDropDownView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val view = super.getDropDownView(position, convertView, parent) as TextView
+                if (position == 0) {
+                    view.setTextColor(Color.GRAY)
+                } else {
+                    view.setTextColor(Color.BLACK)
+                }
+                return view
+            }
+        }
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val textView = view as? TextView
+                if (position == 0) {
+                    textView?.setTextColor(Color.GRAY)
+                } else {
+                    textView?.setTextColor(Color.BLACK)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+        spinner.setSelection(0)
     }
 
 }
+
